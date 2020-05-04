249d745add0febed245717bbb9028fa460b0b9df
Marcin Wojcik <marcincyniu@gmail.com>
2020-05-04 11:44:28 -0700

    Updates

---
+++ precache-manifest.3e9d9b51cf7c4f4a48562c9458a35225.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6c4b87356194a4f5843f8289b13d7b7",
+    "url": "/nbp_currency_reactjs/index.html"
+  },
+  {
+    "revision": "7e995470fd3563ee4752",
+    "url": "/nbp_currency_reactjs/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0051c001b36235ebfe0e",
+    "url": "/nbp_currency_reactjs/static/js/2.139a4541.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nbp_currency_reactjs/static/js/2.139a4541.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e995470fd3563ee4752",
+    "url": "/nbp_currency_reactjs/static/js/main.d9588c84.chunk.js"
+  },
+  {
+    "revision": "2e052e929a3c41b368a5",
+    "url": "/nbp_currency_reactjs/static/js/runtime-main.a10fcbde.js"
+  }
+]);
